66d4f2aa35aeda307a9b89046965f3b5b1604c7c
RozenAstrayChen <rozendhyan@gmail.com>
2020-08-13 08:56:43 +0800

    add position

--- ddpg-v2/rlDDPG.py
+++ ddpg-v2/rlDDPG.py
@@ -4,8 +4,6 @@ Created on Thu Jan 31 14:02:53 2019
 
 @author: Cheng_Home
 """
-
-
 """
 Deep Deterministic Policy Gradient (DDPG), Reinforcement Learning.
 DDPG is Actor Critic based algorithm.
@@ -21,10 +19,12 @@ import math
 import tensorflow as tf
 import numpy as np
 import time
-import RemoteAPIs.vrep as vrep
+import sys
 import matplotlib.pyplot as plt
 import os
 import shutil
+sys.path.append('../')
+import RemoteAPIs.vrep as vrep
 np.random.seed(1)
 # reset the radom seed of tensorflow
 tf.set_random_seed(1)
@@ -52,10 +52,10 @@ if clientID != -1:
 #####################  hyper parameters  ####################
 
 MAX_EPISODES = 15000
-LR_A = 0.0001    # learning rate for actor
-LR_C = 0.001    # learning rate for critic
-GAMMA = 0.9     # reward discount
-TAU = 0.01      # soft replacement
+LR_A = 0.0001  # learning rate for actor
+LR_C = 0.001  # learning rate for critic
+GAMMA = 0.9  # reward discount
+TAU = 0.01  # soft replacement
 MEMORY_CAPACITY = 800000
 BATCH_SIZE = 256
 
@@ -63,9 +63,14 @@ BATCH_SIZE = 256
 
 
 class DDPG(object):
-    def __init__(self, a_dim, s_dim, a_bound, outputGraph=True, saveModel=True):
-        self.memory = np.zeros(
-            (MEMORY_CAPACITY, s_dim * 2 + a_dim * 2), dtype=np.float32)
+    def __init__(self,
+                 a_dim,
+                 s_dim,
+                 a_bound,
+                 outputGraph=True,
+                 saveModel=True):
+        self.memory = np.zeros((MEMORY_CAPACITY, s_dim * 2 + a_dim * 2),
+                               dtype=np.float32)
         self.pointer = 0
         self.best_r = 0
 
@@ -85,25 +90,28 @@ class DDPG(object):
             q_ = self._build_c(self.S_, a_, scope='target', trainable=False)
 
         # networks parameters
-        self.ae_params = tf.get_collection(
-            tf.GraphKeys.GLOBAL_VARIABLES, scope='Actor/eval')
-        self.at_params = tf.get_collection(
-            tf.GraphKeys.GLOBAL_VARIABLES, scope='Actor/target')
-        self.ce_params = tf.get_collection(
-            tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/eval')
-        self.ct_params = tf.get_collection(
-            tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/target')
+        self.ae_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,
+                                           scope='Actor/eval')
+        self.at_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,
+                                           scope='Actor/target')
+        self.ce_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,
+                                           scope='Critic/eval')
+        self.ct_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,
+                                           scope='Critic/target')
 
         #  soft replacement
-        self.soft_replace = [tf.assign(t, (1 - TAU) * t + TAU * e)
-                             for t, e in zip(self.at_params + self.ct_params, self.ae_params + self.ce_params)]
+        self.soft_replace = [
+            tf.assign(t, (1 - TAU) * t + TAU * e)
+            for t, e in zip(self.at_params + self.ct_params, self.ae_params +
+                            self.ce_params)
+        ]
 
-        a_loss = - tf.reduce_mean(q, name="a_loss")    # maximize the q
+        a_loss = -tf.reduce_mean(q, name="a_loss")  # maximize the q
         with tf.variable_scope("td_error"):
             q_target = self.R + GAMMA * q_
             # in the feed_dic for the td_error, the self.a should change to actions in memory
-            td_error = tf.losses.mean_squared_error(
-                labels=q_target, predictions=q)
+            td_error = tf.losses.mean_squared_error(labels=q_target,
+                                                    predictions=q)
 
         with tf.variable_scope("Train"):
             update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
@@ -129,12 +137,16 @@ class DDPG(object):
         indices = np.random.choice(MEMORY_CAPACITY, size=BATCH_SIZE)
         bt = self.memory[indices, :]
         bs = bt[:, :self.s_dim]
-        ba = bt[:, self.s_dim: self.s_dim + self.a_dim]
-        br = bt[:, -self.s_dim - 1: -self.s_dim]
+        ba = bt[:, self.s_dim:self.s_dim + self.a_dim]
+        br = bt[:, -self.s_dim - 1:-self.s_dim]
         bs_ = bt[:, -self.s_dim:]
 
-        self.sess.run([self.ctrain], {self.S: bs,
-                                      self.a: ba, self.R: br, self.S_: bs_})
+        self.sess.run([self.ctrain], {
+            self.S: bs,
+            self.a: ba,
+            self.R: br,
+            self.S_: bs_
+        })
         self.sess.run([self.atrain], {self.S: bs})
 
         # soft target replacement
@@ -153,16 +165,33 @@ class DDPG(object):
         hidden1 = 128
         hidden2 = 64
         with tf.variable_scope(scope):
-            layer1 = tf.layers.dense(s, hidden1, activation=None, kernel_initializer=init_w,
-                                     bias_initializer=init_b, name='l1', trainable=trainable)
-            layer1 = tf.layers.batch_normalization(
-                layer1, training=trainable, name="norms1")
-            layer2 = tf.layers.dense(layer1, hidden2, activation=None, kernel_initializer=init_w,
-                                     bias_initializer=init_b, name='l2', trainable=trainable)
-            layer2 = tf.layers.batch_normalization(
-                layer2, training=trainable, name="norms2")
-            a = tf.layers.dense(layer2, self.a_dim, activation=tf.nn.tanh, name='a',
-                                kernel_initializer=init_w, bias_initializer=init_b, trainable=trainable)
+            layer1 = tf.layers.dense(s,
+                                     hidden1,
+                                     activation=None,
+                                     kernel_initializer=init_w,
+                                     bias_initializer=init_b,
+                                     name='l1',
+                                     trainable=trainable)
+            layer1 = tf.layers.batch_normalization(layer1,
+                                                   training=trainable,
+                                                   name="norms1")
+            layer2 = tf.layers.dense(layer1,
+                                     hidden2,
+                                     activation=None,
+                                     kernel_initializer=init_w,
+                                     bias_initializer=init_b,
+                                     name='l2',
+                                     trainable=trainable)
+            layer2 = tf.layers.batch_normalization(layer2,
+                                                   training=trainable,
+                                                   name="norms2")
+            a = tf.layers.dense(layer2,
+                                self.a_dim,
+                                activation=tf.nn.tanh,
+                                name='a',
+                                kernel_initializer=init_w,
+                                bias_initializer=init_b,
+                                trainable=trainable)
             return tf.multiply(a, self.a_bound, name='scaled_a')
 
     def _build_c(self, s, a, scope, trainable):
@@ -171,36 +200,63 @@ class DDPG(object):
         with tf.variable_scope(scope):
             hidden1 = 256
             hidden2 = 128
-            layer_s = tf.layers.dense(s, hidden1, activation=None, kernel_initializer=init_w,
-                                      bias_initializer=init_b, name='l1_s', trainable=trainable)
-            layer_s = tf.layers.batch_normalization(
-                layer_s, training=trainable, name="norms1")
-            layer_s = tf.layers.dense(layer_s, hidden2, activation=None, kernel_initializer=init_w,
-                                      bias_initializer=init_b, name='l2_s', trainable=trainable)
-            layer_s = tf.layers.batch_normalization(
-                layer_s, training=trainable, name="norms2")
-            layer_a = tf.layers.dense(
-                a, hidden2, activation=None, name='a', trainable=trainable)
+            layer_s = tf.layers.dense(s,
+                                      hidden1,
+                                      activation=None,
+                                      kernel_initializer=init_w,
+                                      bias_initializer=init_b,
+                                      name='l1_s',
+                                      trainable=trainable)
+            layer_s = tf.layers.batch_normalization(layer_s,
+                                                    training=trainable,
+                                                    name="norms1")
+            layer_s = tf.layers.dense(layer_s,
+                                      hidden2,
+                                      activation=None,
+                                      kernel_initializer=init_w,
+                                      bias_initializer=init_b,
+                                      name='l2_s',
+                                      trainable=trainable)
+            layer_s = tf.layers.batch_normalization(layer_s,
+                                                    training=trainable,
+                                                    name="norms2")
+            layer_a = tf.layers.dense(a,
+                                      hidden2,
+                                      activation=None,
+                                      name='a',
+                                      trainable=trainable)
             net = tf.layers.dense(tf.keras.layers.concatenate(
-                [layer_s, layer_a]), hidden2, activation=tf.nn.relu, name='cat', kernel_initializer=init_w, bias_initializer=init_b, trainable=trainable)
-            net = tf.layers.dense(net, self.a_dim, activation=None, name='Q',
-                                  kernel_initializer=init_w, bias_initializer=init_b, trainable=trainable)
+                [layer_s, layer_a]),
+                                  hidden2,
+                                  activation=tf.nn.relu,
+                                  name='cat',
+                                  kernel_initializer=init_w,
+                                  bias_initializer=init_b,
+                                  trainable=trainable)
+            net = tf.layers.dense(net,
+                                  self.a_dim,
+                                  activation=None,
+                                  name='Q',
+                                  kernel_initializer=init_w,
+                                  bias_initializer=init_b,
+                                  trainable=trainable)
             return net
 
     def save_Model(self, DIR, reward, episode):
-        print(
-            "best_reward:{:+.3f}, reward:{:+.3f}".format(self.best_r, reward))
+        print("best_reward:{:+.3f}, reward:{:+.3f}".format(
+            self.best_r, reward))
         if self.best_r < reward:
             print("Model save!")
             modelName = datetime.datetime.now().strftime("%y_%m_%d")
-            self.saver.save(self.sess, DIR+modelName, global_step=episode)
+            self.saver.save(self.sess, DIR + modelName, global_step=episode)
             self.best_r = reward
 
     def load_Model(self, DIR, load=False):
         if load:
             if len(os.listdir(DIR)) == 0:
                 print(
-                    "There is not any model in the {} ! Restart trainning...\n".format(DIR))
+                    "There is not any model in the {} ! Restart trainning...\n"
+                    .format(DIR))
                 time.sleep(1)
                 return False
             else:
@@ -216,7 +272,13 @@ class DDPG(object):
 
 
 class OUNoise(object):
-    def __init__(self, action_space=[], mu=0.0, theta=0.15, max_sigma=0.3, min_sigma=0.3, decay_period=100000):
+    def __init__(self,
+                 action_space=[],
+                 mu=0.0,
+                 theta=0.15,
+                 max_sigma=0.3,
+                 min_sigma=0.3,
+                 decay_period=100000):
         self.mu = mu
         self.theta = theta
         self.sigma = max_sigma
@@ -244,6 +306,7 @@ class OUNoise(object):
             (self.max_sigma - self.min_sigma) * min(1.0, t / self.decay_period)
         return np.clip(action + ou_state, self.low, self.high)
 
+
 ###############################  function  ####################################
 
 
@@ -259,48 +322,51 @@ def step(action, lineNum):
     # vrep.simx_opmode means that sending the action once in one single time
     vrep.simxSetFloatSignal(clientID, "action", action,
                             vrep.simx_opmode_blocking)
-    vrep.simxSetIntegerSignal(
-        clientID, "line", lineNum, vrep.simx_opmode_blocking)
+    vrep.simxSetIntegerSignal(clientID, "line", lineNum,
+                              vrep.simx_opmode_blocking)
     # activate the synchronous mode
     vrep.simxSynchronousTrigger(clientID)
     vrep.simxGetPingTime(clientID)
     # from the buffer retrieve the information which involves the velocity, angle, reward ,and the terminated state
-    error, next_angle = vrep.simxGetFloatSignal(
-        clientID, "angle", vrep.simx_opmode_buffer)
-    error, next_velocity = vrep.simxGetFloatSignal(
-        clientID, "velocity", vrep.simx_opmode_buffer)
-    error, reward = vrep.simxGetFloatSignal(
-        clientID, "reward", vrep.simx_opmode_buffer)
-    error, done = vrep.simxGetIntegerSignal(
-        clientID, "done", vrep.simx_opmode_buffer)
+    error, next_angle = vrep.simxGetFloatSignal(clientID, "angle",
+                                                vrep.simx_opmode_buffer)
+    error, next_velocity = vrep.simxGetFloatSignal(clientID, "velocity",
+                                                   vrep.simx_opmode_buffer)
+    error, reward = vrep.simxGetFloatSignal(clientID, "reward",
+                                            vrep.simx_opmode_buffer)
+    error, done = vrep.simxGetIntegerSignal(clientID, "done",
+                                            vrep.simx_opmode_buffer)
     # integrate the angle and the velocity to the next obs
     observation_ = np.array([next_angle, next_velocity], dtype=np.float32)
-    print("lineNum{}|  angle:{:+.3f}  velocity:{:+.3f}  reward:{:+.3f}  done:{}".format(
-        str(lineNum).zfill(3), next_angle*180/math.pi, next_velocity*180/math.pi, reward, done))
+    print(
+        "lineNum{}|  angle:{:+.3f}  velocity:{:+.3f}  reward:{:+.3f}  done:{}".
+        format(
+            str(lineNum).zfill(3), next_angle * 180 / math.pi,
+            next_velocity * 180 / math.pi, reward, done))
     return observation_, reward, done
 
 
 def reset():
     # extract the first state from the vrep
     vrep.simxSetIntegerSignal(clientID, "line", 0, vrep.simx_opmode_blocking)
-    error, angle = vrep.simxGetFloatSignal(
-        clientID, "angle", vrep.simx_opmode_buffer)
-    error, velocity = vrep.simxGetFloatSignal(
-        clientID, "velocity", vrep.simx_opmode_buffer)
+    error, angle = vrep.simxGetFloatSignal(clientID, "angle",
+                                           vrep.simx_opmode_buffer)
+    error, velocity = vrep.simxGetFloatSignal(clientID, "velocity",
+                                              vrep.simx_opmode_buffer)
     obs = np.array([angle, velocity], dtype=np.float32)
     return obs
 
 
 def stream():
     # prerequisite of retrieving the data from the buffer: declartion the mode in streaming
-    error, next_angle = vrep.simxGetFloatSignal(
-        clientID, "angle", vrep.simx_opmode_streaming)
-    error, next_velocity = vrep.simxGetFloatSignal(
-        clientID, "velocity", vrep.simx_opmode_streaming)
-    error, reward = vrep.simxGetFloatSignal(
-        clientID, "reward", vrep.simx_opmode_streaming)
-    error, done = vrep.simxGetIntegerSignal(
-        clientID, "done", vrep.simx_opmode_streaming)
+    error, next_angle = vrep.simxGetFloatSignal(clientID, "angle",
+                                                vrep.simx_opmode_streaming)
+    error, next_velocity = vrep.simxGetFloatSignal(clientID, "velocity",
+                                                   vrep.simx_opmode_streaming)
+    error, reward = vrep.simxGetFloatSignal(clientID, "reward",
+                                            vrep.simx_opmode_streaming)
+    error, done = vrep.simxGetIntegerSignal(clientID, "done",
+                                            vrep.simx_opmode_streaming)
 
 
 def visualize(EPISODE, Reward):
@@ -312,8 +378,8 @@ def visualize(EPISODE, Reward):
     plt.grid()
     plt.show()
 
-####################  Main function  #########################
 
+####################  Main function  #########################
 
 if __name__ == '__main__':
     # sate: angle and velocity
@@ -334,12 +400,12 @@ if __name__ == '__main__':
         for episode in range(MAX_EPISODES):
             while True:
                 # poll the useless signal (to receive a message from server)
-                vrep.simxGetIntegerSignal(
-                    clientID, 'line', vrep.simx_opmode_blocking)
+                vrep.simxGetIntegerSignal(clientID, 'line',
+                                          vrep.simx_opmode_blocking)
 
                 # check server state (within the received message)
-                e = vrep.simxGetInMessageInfo(clientID,
-                                              vrep.simx_headeroffset_server_state)
+                e = vrep.simxGetInMessageInfo(
+                    clientID, vrep.simx_headeroffset_server_state)
 
                 # check bit0
                 not_stopped = e[1] & 1
@@ -348,7 +414,8 @@ if __name__ == '__main__':
                     break
                 else:
                     print('not_stopped')
-                    vrep.simxStopSimulation(clientID, vrep.simx_opmode_blocking)
+                    vrep.simxStopSimulation(clientID,
+                                            vrep.simx_opmode_blocking)
 
             # IMPORTANT
             # you should always call simxSynchronous()
@@ -388,7 +455,8 @@ if __name__ == '__main__':
                     REWARD.append(R)
                     EPISODE.append(episode)
                     # stop the simulation
-                    vrep.simxStopSimulation(clientID, vrep.simx_opmode_blocking)
+                    vrep.simxStopSimulation(clientID,
+                                            vrep.simx_opmode_blocking)
                     time.sleep(1.2)
                     break
                 s = s_
--- ddpg-v2/sim_test.py
+++ ddpg-v2/sim_test.py
@@ -1,54 +1,19 @@
-import vrep
+import sys
+sys.path.append('../')
+import RemoteAPIs.vrep as vrep
 clientID = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
-if clientID != -1:
-    print('Connected to remote API server')
+vrep.simxSynchronous(clientID, True)
+print(clientID)
+def get_shape_position(name):
+    err1, handle1 = vrep.simxGetObjectHandle(clientID, name,
+                                             vrep.simx_opmode_blocking)
+    err2, handle2 = vrep.simxGetObjectHandle(clientID, 'IRB140_joint1',
+                                             vrep.simx_opmode_blocking)
+    #vrep.simxStartSimulation(clientID, vrep.simx_opmode_blocking)
 
-       # setup a useless signal
-    vrep.simxSetIntegerSignal(clientID, 'line', 1, vrep.simx_opmode_oneshot)
+    pos = vrep.simxGetObjectPosition(clientID, handle2, -1,
+                                     vrep.simx_opmode_streaming)
+    print(pos)
 
-    for j in range(5):
-        print('---------------------simulation', j)
 
-        # IMPORTANT
-        # you should poll the server state to make sure
-        # the simulation completely stops before starting a new one
-        while True:
-            # poll the useless signal (to receive a message from server)
-            vrep.simxGetIntegerSignal(
-                clientID, 'line', vrep.simx_opmode_oneshot)
-
-            # check server state (within the received message)
-            e = vrep.simxGetInMessageInfo(clientID,
-                                          vrep.simx_headeroffset_server_state)
-
-            # check bit0
-            not_stopped = e[1] & 1
-
-            if not not_stopped:
-                break
-            else:
-                print('not_stopped')
-                vrep.simxStopSimulation(clientID, vrep.simx_opmode_oneshot)
-
-        # IMPORTANT
-        # you should always call simxSynchronous()
-        # before simxStartSimulation()
-        vrep.simxSynchronous(clientID, True)
-        # then start the simulation:
-        e = vrep.simxStartSimulation(clientID, vrep.simx_opmode_oneshot)
-        print('start', e)
-
-        # Now step a few times:
-        for i in range(2):
-            e = vrep.simxSynchronousTrigger(clientID)
-            print('synct', e)
-            # wait till simulation step finish
-            e = vrep.simxGetPingTime(clientID)
-            print('getping', e)
-
-        # stop the simulation:
-        vrep.simxStopSimulation(clientID, vrep.simx_opmode_oneshot)
-        print('stop')
-
-    # Now close the connection to V-REP:
-    vrep.simxFinish(clientID)
+get_shape_position('Cuboid')
